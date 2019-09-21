9c45e9aef78c7a0af9b6609bb16e7b1d6d67c36b
takowasabi <tomioka2012@gmail.com>
2019-09-21 20:42:09 +0900

    add four frames

---
+++ Assets/Scripts/Tomioka/Frame/FourFrames.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourFrames : MonoBehaviour
+{
+    [SerializeField]
+    FrameRepository frameRepository;
+
+    [SerializeField]
+    SpriteRenderer kiFrameViews;
+    [SerializeField]
+    SpriteRenderer shoFrameViews;
+    [SerializeField]
+    SpriteRenderer tenFrameViews;
+    [SerializeField]
+    SpriteRenderer ketsuFrameViews;
+
+    List<FrameID> frameIDs = new List<FrameID>(4);
+
+    public void AddFrame(FrameID frameID)
+    {
+        switch(frameRepository.GetSituation(frameID))
+        {
+            case SituationID.Ki:
+                if (kiFrameViews.sprite == null)
+                {
+                    kiFrameViews.sprite = frameRepository.GetFrameView(frameID);
+                    frameIDs[0] = frameID;
+                }
+                break;
+            case SituationID.Sho:
+                if (shoFrameViews.sprite == null)
+                {
+                    shoFrameViews.sprite = frameRepository.GetFrameView(frameID);
+                    frameIDs[1] = frameID;
+                }
+                break;
+            case SituationID.Ten:
+                if (tenFrameViews.sprite == null)
+                {
+                    tenFrameViews.sprite = frameRepository.GetFrameView(frameID);
+                    frameIDs[2] = frameID;
+                }
+                break;
+            case SituationID.Ketsu:
+                if (ketsuFrameViews.sprite == null)
+                {
+                    ketsuFrameViews.sprite = frameRepository.GetFrameView(frameID);
+                    frameIDs[3] = frameID;
+                }
+                break;
+        }
+    }
+
+    public void ResetFrames()
+    {
+        frameIDs = new List<FrameID>(4);
+        kiFrameViews.sprite = null;
+        shoFrameViews.sprite = null;
+        tenFrameViews.sprite = null;
+        ketsuFrameViews.sprite = null;
+    }
+
+    public 
+}
